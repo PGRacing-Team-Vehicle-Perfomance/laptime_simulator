99ebeec104b6cc20c808291d835e56c11e2cf20f
Stanisław Grochowski <grochowski0stanislaw@gmail.com>
2025-12-30 20:14:34 +0100

    wip

--- src/vehicle/vehicle.h
+++ src/vehicle/vehicle.h
@@ -1,13 +1,52 @@
 #pragma once
 
 #include "config/config.h"
+#include "vehicle/body.h"
 #include "vehicle/tire/tire.h"
 #include "vehicle/vehicleHelper.h"
 
 class Vehicle {
-    VehicleConfig config;
+    struct {
+        std::vector<std::unique_ptr<Body>> chacieElements;
+        std::vector<std::unique_ptr<AeroElement>> aeroElements;
+        std::vector<std::unique_ptr<Body>> wheelAssemblyElements;
+        std::vector<std::unique_ptr<Body>> suspentionElements;
+        CarWheelBase<std::unique_ptr<Tire>> tires;
+        Body engine;
+    } parts;
 
+    struct {
+        vec3<float> roll;
+        vec3<float> pitch;
+        vec3<float> yaw;
+    } degOfFreedomCenter;
+
+    struct {
+        vec3<float> linear;
+        vec3<float> angular;
+    } velocity;
+
+    const float rollCenterHeightFront;
+    const float rollCenterHeightBack;
+    const float antiRollStiffnessFront = 15000.0;
+    const float antiRollStiffnessRear = 15000.0;
+    const float frontTrackWidth = 1.256;
+    const float rearTrackWidth = 1.216;
+
+    Body combinedTotalMass;
+    Body combinedSuspendedMass;
+    Body combinedUnsuspendedMass;
+
+    float combinedCla;
+    vec3<float> combinedClaPosition;
+
+    float cda = 1.3;
+    float crr = 0.015;
+    float wheelbase = 1.53;
+
+    VehicleConfig config;
     CarWheelBase<std::unique_ptr<Tire>> tires;
+
     CarWheelBase<float> distributeForces(float totalForce, float frontDist, float leftDist);
     CarWheelBase<float> totalTireLoads(float velocity, vec2<float> acceleration,
                                        const SimConfig& simConfig);
@@ -16,7 +55,6 @@ class Vehicle {
     CarWheelBase<float> loadTransfer(vec2<float> acceleration);
     float getEngineTorque(float rpm);
     float getWheelTorque(float engine_torque, int gear);
-    const float totalMass;
 
    public:
     Vehicle(VehicleConfig config);
